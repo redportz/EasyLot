fcfe3339bffa66e32fbc12d0306f0a759eb3213e
redportz <120698710+redportz@users.noreply.github.com>
2025-10-10 01:49:13 -0400

    style landing page

---
+++ js/rotate-arrows.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('li.location').forEach(function(location) {
+    location.addEventListener('click', function() {
+        this.classList.toggle('open');
+    });
+});
+
+
+var accordions = document.getElementsByClassName("accordion");
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
